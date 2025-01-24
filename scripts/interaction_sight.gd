4043758e5dd7c257a45baadbad1f7c6b9197b4b1
Louskil <tbikulov@gmail.com>
2025-01-24 14:57:33 +0200

    Added door teleport prototype

--- scripts/interaction_sight.gd
+++ scripts/interaction_sight.gd
@@ -7,15 +7,27 @@ class_name Interaction_rc
 func _physics_process(delta: float) -> void:
 	if self.is_colliding():
 		var collider = self.get_collider()
-		material = collider.get_node("../../").outline_material
-		overlay = collider.get_node("../")
-		overlay.material_overlay = material
-		
+		#material = collider.get_node("../../").outline_material
+		#overlay = collider.get_node("../")
+		var layer = collider.get_collision_layer()
+		#overlay.material_overlay = material
+		#print(layer)
+		if layer == 4:
+			material = collider.get_node("../../").outline_material
+			overlay = collider.get_node("../")
+			overlay.material_overlay = material
+			
+		# Xyeta pro predmet i teleport
 		if Input.is_action_just_pressed("interact"):
-			var object = collider.get_node("../../")
 			var character: Character = self.get_node("../../")
-			character.inventory.add_item(object.item)
-			object.queue_free()
+			if layer == 4:
+				var object = collider.get_node("../../")
+				character.inventory.add_item(object.item)
+				object.queue_free()
+			else:
+				var marker: Marker3D = self.get_node("../../../TestTeleport") 
+				character.position = marker.position
+			
 			
 	else:
 		if overlay != null:
